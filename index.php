b3260eb77bdf2f5fca47f43f353247be64d52c13
Subhash Singh <subhash_singh@seologistics.com>
2022-07-11 15:56:02 +0530

    Smtp Code

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+
+//====================SMTP-Configuration=====================================================//    
+
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'support.tech@gmail.com';                     //SMTP username
+    $mail->Password   = 'acvuebxcyltpqmun';                               //SMTP password
+    $mail->SMTPSecure = "TLS"; 				           //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    //Recipients
+    $mail->setFrom('support.tech@gmail.com', 'Mailer');
+    $mail->addAddress('support.tech@gmail.com', 'Joe User');     //Add a recipient
+  
+//====================SMTP-Configuration=====================================================//    
+
+
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Checking SMTP Credential';
+    $mail->Body    = 'This message is genrated by testing purpose, Please ignore <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Your Message has been sucessfully sent';
+
+
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
